95a1ff6a81723299bf786f8693422716c9920bd6
K-Kizuku <kotani.kizuku@gmail.com>
2023-03-27 11:31:41 +0900

    Merge branch 'main' of github.com:K-Kizuku/V_DOM_framework

---
+++ src/app/pages/temp.ts
@@ -0,0 +1,13 @@
+import { createVElement } from "@/framework";
+export const temp = createVElement(
+  "div",
+  { class: "temp" },
+  createVElement("div", { class: "tata" }, "jifbseuGBu")
+);
+/*
+<div>
+    <div>
+        jifbseuGBu
+    </div>
+</div>
+*/
