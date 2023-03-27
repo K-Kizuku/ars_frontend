e677c287bb444747d7d7cf860f5287ccaa121deb
Sea10wood <miyusea5@gmail.com>
2023-03-27 17:52:24 +0900

    問題ありフッター

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,33 +1,49 @@
-    
-// import { createVElement } from "@/framework";
-
-// export const footer = createVElement(
-//   "div",
-//   { class: "footer" }, 
-//   createVElement("div", {
-//     style: () => {
-//     }
-//       // position: "fixed",
-//       // bottom: "0",
-//       // width: "100%",
-//       // height: "20%",
-//       // left: "0",
-//       // background: "#2f4f4f",
-//       // transform: "rotate(90.27deg)",
-//   } , )
-
-// );
-
 import { createVElement } from "@/framework";
-// import   from "@/img/afsa.png";
+// import  {asfa} from "@/img/afsa.png";
+// import  {asfatext} from "@/img/afsatext.png";
 
 export const footer = createVElement(
   "div",
   { class: "footer" }, 
-    createVElement("div", {
-      style: 'position: "fixed", bottom: "0",width: "100%",height: "40%", background-Color: "#1E1E1E"'
-    } ,
-    createVElement("img", {
-      src: 'img: "../img/afsa.png", posithion: "static", bottom: "0", left: "0", width: "20%", height: "40%", '} 
-  ),)
-);
+      createVElement(
+        "div",  {
+          
+          margin: "0",
+          position: "fixed",
+          bottom: "0",
+          width: "100%",
+          height: "25%", 
+          backgroundColor: "#0000000"
+        } ,
+      createVElement(
+        "img", 
+        {
+        src: '../img/afsa.png', 
+        posithion: "static", 
+        bottom: "0", 
+        left: "0", 
+        width: "20%", 
+        height: "40%", 
+      } ,
+      createVElement("img", {
+        src: '../img/afsatext.png', 
+        position: "static", 
+        bottom: "0", 
+        left: "10%", 
+        width: "20%", 
+        height: "40%", 
+      } ,
+      createVElement(
+        "h2", {
+          Text: "全日本フルスクラッチ協会",
+          position: "statics",
+          bottom: "0",
+          left: "10%",
+          color: "#000000"
+          
+      } ,
+    ),
+    ),
+    ),
+      ),
+)
