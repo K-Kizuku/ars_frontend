f9163ef2fb1235bb08e406a767f2e0e56c24f01c
kawasemi256 <1201okariku@gmail.com>
2023-03-28 06:09:10 +0900

    [edi]css

--- src/app/pages/myTalkRoom.ts
+++ src/app/pages/myTalkRoom.ts
@@ -3,47 +3,51 @@ import { createVElement } from "@/framework";
 // ページ名はMyTalkRoom
 export const myTalkRoom = createVElement(
     "body",
-    { class: "body" },
+    { class: "body",style:"background-color: #D9D9D9;" },
     createVElement(
         "main",
         { class: "main" },
         createVElement(//上部見出し
             "div",
-            {},
+            {
+                style:"display: flex; ;" 
+            },
             createVElement(
                 "img",
-                { src:"" }//自分のアイコンの写真
+                { src:"src/img/logo.png" ,alt:"usericon", style:"margin:1rem 2rem;"}//userのアイコンを表示予定
             ),
             createVElement(
                 "h1",
-                {},
+                { style:"margin:auto 2rem;" },
                 "My Talk Room"
             )
         ),
         createVElement(//投稿作成の場所
             "div",
-            {},
+            { style:"flex-direction: column;" },
             createVElement(
                 "input",
                 { 
                 type:"text",
                 name:"title",
                 placeholder:"タイトルを入力",
+                style:"line-height:3rem; width:60%; margin:1rem auto 0rem 2rem;  ",
                 },
             ),
             createVElement(
-                "input",
+                "textarea",
                 {
-                type:"text",
-                name:"text",
-                placeholder:"投稿内容を入力"
+                placeholder:"投稿内容を入力",
+                wrap:"soft",
+                style:"height:10rem; width:80%; margin:1rem auto 0rem 2rem;",
                 }
             ),
             createVElement(
                 "input",
                 {
                 type:"submit",
-                value:"投稿"
+                value:"投稿",
+                style:"font-size: large;line-height:5rem; width:30%;margin:1rem auto 1rem 2rem;"
                 },
             )
 
