6345965778574642baf733cdca9a4bc85a85593c
kawasemi256 <1201okariku@gmail.com>
2023-03-27 17:52:03 +0900

    [add]selection.ts

---
+++ src/app/pages/selection.ts
@@ -0,0 +1,53 @@
+import { createVElement } from "@/framework";
+
+export const selection = createVElement(
+    "body",
+    { class: "body" },
+    createVElement(
+        "main",
+        { class: "main" },
+        createVElement(
+            "div",
+            { class:"yakishamopng" },
+            createVElement(
+                "img",
+                { src:"" } //やきしゃもさんの写真
+            ),
+        ),
+        createVElement(
+            "div",
+            {class:"text"},
+            createVElement(
+                "h1",
+                {},
+                "ようこそ"
+            ),
+            createVElement(
+                "h1",
+                {},
+                "全日本フルスクラッチ協会へ。"
+            ),
+            createVElement(
+                "h2",
+                {},
+                "全て書けば全て分かる"
+            ),
+        ),
+        createVElement(
+            "a",
+            { href:"" },//新規登録のurl
+            createVElement(
+                "img",
+                { src: ""},//新規登録のimage
+            )
+        ),
+        createVElement(
+            "a",
+            {href:""},//ログインのurl
+            createVElement(
+                "img",
+                { src:"" },//ログインのurl
+            )
+        )
+    )
+);
